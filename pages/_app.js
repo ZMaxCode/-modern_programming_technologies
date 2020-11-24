34559beb452501eb1b109dea32046ca184b17af8
zmaxcode <promakc31@gmail.com>
2020-11-24 19:28:51 +0800

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,17 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import 'primeflex/primeflex.css';
+
+import 'primereact/resources/themes/bootstrap4-light-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+
+import Router from 'next/router';
+import NProgress from 'nprogress'; 
+import 'nprogress/nprogress.css'; 
+
+Router.events.on('routeChangeStart', () => NProgress.start()); 
+Router.events.on('routeChangeComplete', () => NProgress.done()); 
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
